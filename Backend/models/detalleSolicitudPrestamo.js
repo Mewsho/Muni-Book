8c33ed9ffa1ab983891402712d4bb990981890d0
PadoruHeleo <fco.torres01@gmail.com>
2024-10-17 20:52:59 -0300

    Query y Mutation
    - Query y mutations basico para:
    Ejemplar, DetalleSolicitudPrestamo y Documento

--- Backend/models/detalleSolicitudPrestamo.js
+++ Backend/models/detalleSolicitudPrestamo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const detalleSolicitudPrestamoSchema = mongoose.Schema({
     ejemplares : [{ type: mongoose.Schema.Types.ObjectId, ref : 'Documento'}],
-    solicitud : { type: mongoose.Schema.Types.ObjectId, ref : 'SolicitudPrestamo'}
+    solicitudPrestamo : { type: mongoose.Schema.Types.ObjectId, ref : 'SolicitudPrestamo'}
 });
 
 module.exports = mongoose.model('DetalleSolictudPrestamo', detalleSolicitudPrestamoSchema); 
